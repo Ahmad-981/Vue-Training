96d33c232feb5cc5e2ea059364cf3feb1ccceec7
AHMAD <89203329+Ahmad-981@users.noreply.github.com>
2024-09-10 10:43:26 +0500

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { defineCustomElement } from 'vue';
 import MyComponent from './components/MyComponent.vue';
 
 // Create a custom element from the Vue component
-const MyVueComponent = defineCustomElement(MyComponent, {shadow: true});
+const MyVueComponent = defineCustomElement(MyComponent);
 
 // Define the custom element
 customElements.define('my-vue-component', MyVueComponent);
